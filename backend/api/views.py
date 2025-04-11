29b77181c798116374a430b664be109ba3b0c4bb
Shivam Shukla <shukla.shivamv@gmail.com>
2025-04-12 02:04:51 +0530

    Working Prototype with jwt authentication, django setup, pre requisets added

---
+++ backend/api/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import status
+from .serializers import RegisterSerializer
+
+# Register API
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+# Protected route
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def protected_view(request):
+    return Response({"message": f"Hello, {request.user.username}! This is a protected view."})
+
